cd00abf9fba44bbba7e7be576252eb98773bfa74
dangaiden <dbalcaraz@gmail.com>
2021-10-18 19:17:42 +0200

    Adding new Functions v2

--- app/server.go
+++ app/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 
@@ -24,7 +25,27 @@ func entry(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Endpoint Hit: WelcomePage")
 }
 
-// Function to return an specific object.
+/*
+Function that parses the data received and appends
+it to the Pokemons array that we have created
+*/
+func newPokemon(w http.ResponseWriter, r *http.Request) {
+	reqBody, _ := ioutil.ReadAll(r.Body)
+
+	var pokemon Pokemon
+	json.Unmarshal(reqBody, &pokemon)
+
+	Pokemons = append(Pokemons, pokemon)
+	json.NewEncoder(w).Encode(pokemon)
+}
+
+// Return all objects (pokemons)
+func returnPokemons(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Endpoint Hit: returnPokemons")
+	json.NewEncoder(w).Encode(Pokemons)
+}
+
+// Function to return an specific object (pokemon).
 func returnPokemon(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["Number"]
@@ -36,38 +57,30 @@ func returnPokemon(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// Return all pokemons
-func returnPokemons(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Endpoint Hit: returnPokemons")
-	json.NewEncoder(w).Encode(Pokemons)
-}
-
+/*
+This is our "proxy" (router) function which will redirect each
+request to the corresponding Function.
+*/
 func handleRequests() {
 	PrimaryRouter := mux.NewRouter().StrictSlash(true)
-	PrimaryRouter.HandleFunc("/", entry)
-	PrimaryRouter.PathPrefix("/go").Handler(http.FileServer(http.Dir("./static")))
+	//PrimaryRouter.HandleFunc("/", entry)
+	PrimaryRouter.PathPrefix("/").Handler(http.FileServer(http.Dir("./static")))
 	PrimaryRouter.HandleFunc("/all", returnPokemons)
+	PrimaryRouter.HandleFunc("/pokemon/{Number}", returnPokemon)
+	PrimaryRouter.HandleFunc("/pokemon", newPokemon).Methods("POST")
 
 	log.Fatal(http.ListenAndServe(":8080", PrimaryRouter))
 
 }
 
+// Main function
 func main() {
-
+	// We define our array with some entries.
 	Pokemons = []Pokemon{
 		Pokemon{Name: "Dragonite", Number: "149", Type: "Dragon"},
 		Pokemon{Name: "Aerodactyl", Number: "142", Type: "Rock"},
 		Pokemon{Name: "Charmander", Number: "004", Type: "Fire"},
 	}
-
-	//	fileServer := http.FileServer(http.Dir("./static"))
-	//	http.Handle("/go", fileServer)
-
-	/* http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "*Hi!!")
-	}) */
-
-	//http.Handle("/", http.FileServer(http.Dir("./static")))
 
 	handleRequests()
 }
