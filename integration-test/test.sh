77489022d44d6593fc8e955876260b7ef6fbd4dd
dangaiden <dbalcaraz@gmail.com>
2021-10-19 02:11:38 +0200

    Modifying tests

--- integration-test/test.sh
+++ integration-test/test.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
+
 set -e
 
 sleep 10
-
-
-all="curl -s https://holded.itgaiden.com/all | q '.'"
-p004 = "curl -s https://holded.itgaiden.com/pokemon/004 | q '.'"
-pname004 = "curl -s https://holded.itgaiden.com/pokemon/004 | q '.Name'"
-
+error=0
 
 echo "Checking localhost or endpoint '/'"
 if $(curl -s -I localhost | grep 200 >/dev/null);
@@ -18,41 +14,59 @@ else
   exit 1
 fi
 
-echo "Checking endpoint '/all'"
-string=$(curl -s localhost/all)
-if [all = $string];
-then
-  echo "Test passed, expected value."
+echo "-- Items list response test --"
+echo "Testing item list response, looking for a valid json list"
+json_string=$(curl -s localhost/all)
+if jq -e . >/dev/null 2>&1 <<<"$json_string"; then
+    echo "Items list response test passed"
 else
-  echo "Not a 200 HTTP status code"
-  exit 1
+    echo "Items list response test failed"
+    error=1
 fi
 
-echo "Checking endpoint '/pokemon/004'"
-if [[ $pname004 -eq "Charmander" ]]; then
-then
-  echo "Test passed, expected value."
+
+echo "-- Single Item query test --"
+echo "Testing single Item query , looking for a valid json list"
+json_string=$(curl -s localhost/pokemon/004)
+if jq -e . >/dev/null 2>&1 <<<"$json_string"; then
+    echo "Single Item query test passed"
 else
-  echo "Not a 200 HTTP status code or wrong expected value"
-  exit 1
+    echo "Single Item query test failed"
+    error=1
 fi
 
-# echo "Checking endpoint '/all'"
-# string=$(curl -s localhost/all)
-# if  <<<"$json_string";
-# then
-#   echo "Test passed"
-# else
-#   echo "Not a 200 HTTP status code"
-#   exit 1
-# fi
+echo "-- Item creation test --"
+echo "Testing Item creation ,looking for a valid json list"
+curl -X POST -d '{"Name":"Pikachu","Number":"025","Type":"Electric"}' \
+ -H "Content-Type: application/json" localhost/pokemon
+json_string=$(curl -s localhost/pokemon/025)
+id=$(echo "$json_string" |  jq -r '.Name')
+if [[ $id -eq "Pikachu" ]]; then
+    echo "Item creation test passed, Pikachu as the expected value"
+else
+    echo "Item creation test failed"
+    error=1
+fi
 
-# echo "Checking endpoint '/all'"
-# string=$(curl -s localhost/all)
-# if  <<<"$json_string";
-# then
-#   echo "Test passed"
-# else
-#   echo "Not a 200 HTTP status code"
-#   exit 1
-# fi
+echo "-- Item deletion test --"
+echo "Testing Item deletion ,looking for a not valid json list"
+curl -s -X DELETE localhost/pokemon/004
+json_string=$(curl -s localhost/004)
+id=$(echo "$json_string" |  jq -r '.Number')
+if [[ $id -eq "004" ]]; then
+    echo "Item deletion test failed"
+    error=1
+else
+    echo "Item deletion test passed"
+fi
+
+
+
+
+
+if [[ $error -eq 1 ]]; then
+  docker-compose -f "testing/docker-compose.yml" down
+  exit 1
+else
+  exit 0
+fi
